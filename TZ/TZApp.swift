29a87ffd689aa7c1a8c3daaa6f7890fcf00d4e4d
VLad <vladbehim@gmail.com>
2025-09-07 09:28:16 +0300

    Initial Commit

---
+++ TZ/TZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZApp.swift
+//  TZ
+//
+//  Created by Vladyslav Behim on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

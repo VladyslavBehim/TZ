6304f7296a765a3dec3301384f448e2b9571ea95
VLad <vladbehim@gmail.com>
2025-09-08 09:30:01 +0300

    game view commit \ Rating view

---
+++ TZ/View/RatingView/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  TZ
+//
+//  Created by Vladyslav Behim on 08.09.2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
